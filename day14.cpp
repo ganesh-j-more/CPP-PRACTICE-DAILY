65ba2a580523e3f83ef9d9e7d1d19985e46226d2
More Ganesh Jitesh <ganeshmore775593@gmail.com>
2025-08-25 11:37:02 +0530

    Practice of Arithmetic Operators in CPP p

---
+++ day14.cpp
@@ -0,0 +1,22 @@
+// C++ Operators Precedence & Associativity o Arithimetic Operator
+// Ex 1)
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int a = 4;
+//   int b = a++ + ++a*2;
+//   cout<<b<<endl;
+//   return 0;
+// }
+// Ex - 2) 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int x = 20;
+  int y = --x + x++*5;
+  cout<<y<<endl;
+  return 0;
+}
