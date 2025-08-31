cef86d901fcbea8b77c37290e87d1ad93a2478ce
More Ganesh Jitesh <ganeshmore775593@gmail.com>
2025-08-31 13:21:02 +0530

    WHILE LOOP

---
+++ day25.cpp
@@ -0,0 +1,16 @@
+// While Loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int pin;
+
+
+  while (pin!=9999)
+  {
+    cout<<"Incorrect Pin. Please Try Again";
+      cin>>pin;
+  }
+  return 0;
+}
