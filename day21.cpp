822d1fbf556289f225f83aa1dcd89ead3a167210
More Ganesh Jitesh <ganeshmore775593@gmail.com>
2025-08-31 13:18:13 +0530

    IF , IF-ELSE, IF ELIF, NESTED IF example pratice

---
+++ day21.cpp
@@ -0,0 +1,106 @@
+// // if statement 
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int a=30;
+//   if (a>20)
+//   {
+//     cout<<"A is Grater then 20";
+//   }
+//   return 0;
+// }
+
+// if-else Statement 
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int age;
+//   cout<<"Enter your age...";
+//   cin>>age;
+//   if (age>18)
+//   {
+//     cout<<"Congratulation,You are Eligible";
+//   }
+//   else
+//   {
+//     cout<<"Sorry, You are not Eligible this Compitation";
+//   }
+//   return 0;
+// }
+
+// if- elif Statement
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//   int marks;
+//   cout<<"Enter your marks";
+//   cin>>marks;
+
+//   if (marks>85)
+//   {
+//     cout<<"A+";
+//   }
+//   else if (marks>75)
+//   {
+//     cout<<"A";
+//   }
+//   else if (marks>50)
+//   {
+//     cout<<"C";
+//   }
+//   else if (marks>35)
+//   {
+//     cout<<"D";
+//   }
+  
+//   else
+//   {
+//     cout<<"Fail";
+//   }
+  
+//  return 0;
+//}
+
+// Nested If 
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//   int pin;
+//   cout<<"Enter your Pin";
+//   cin>>pin;
+  
+//   int amount;
+//   cout<<"enter your Amount";
+//   cin>>amount;
+//     int balence;
+//   cout<<"enter your balence";
+//   cin>>balence;
+  
+
+//   if (pin == 1218)
+//   {
+//     if (amount<=balence)
+//     {
+//       cout<<"Transection Seccuessful...";
+//     }
+//     else{
+//       cout<<" money not found";
+//     }
+//   }
+//   else{
+//     cout<<"erorr";
+
+//   }
+//   return 0;
+// }
