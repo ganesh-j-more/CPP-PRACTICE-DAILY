aa27cd4e42c98aa57ba06902007aead3e2397408
More Ganesh Jitesh <ganeshmore775593@gmail.com>
2025-08-31 13:19:43 +0530

    SWITCH STATEMENT

---
+++ day23.cpp
@@ -0,0 +1,44 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   // int num;
+//   // cout<<"Enter your Number";
+//   // cin>>num;
+
+//   int day;
+//   // cout<<"Enter the number";
+//   // cin>>day;
+
+//   switch (day)
+//   {
+//   case 1: cout<<"Monday";break;
+//   case 2: cout<<"Tuesday";break;
+//   case 3: cout<<"Thursday";break;
+//   case 4: cout<<"Friday";break;
+//   case 5: cout<<"Saturday";break;
+//   case 6: cout<<"Sunday";break;
+//   default:cout<<"Invalid number"; 
+// }
+
+// return 0;
+// }
+
+// MY FRINDS SWITCH STATEMENT
+
+#include<iostream>;
+using namespace std;
+
+int main(){
+  int a = 6;
+  switch (a)
+  {
+  case 1: cout<<"Soma";break;
+  case 2: cout<<"Kishuu";break;
+  case 3: cout<<"Shri";break;
+  case 4: cout<<"Ganu";break;
+  case 5: cout<<"Raj";break;
+  default:cout<<"Invalid NoPlease try Again";
+  }
+  return 0;
+}
